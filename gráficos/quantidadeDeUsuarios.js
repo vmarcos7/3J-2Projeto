04befb0c3ffd09b2c0e97495385dbf9bbc351c02
vmarcos7 <167425877+vmarcos7@users.noreply.github.com>
2024-10-11 09:41:25 -0300

    Add files via upload
    11 outubro

--- gráficos/quantidadeDeUsuarios.js
+++ gráficos/quantidadeDeUsuarios.js
@@ -13,4 +13,10 @@ const infos = [
         type: 'bar'
     }
 ]
+
+const grafico = document.createElement('div');
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico,infos)
 }
+quantidadeDeUsuarios()
